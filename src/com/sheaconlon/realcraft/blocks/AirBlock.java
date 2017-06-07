519e86372d554c4e78a271712749ca440c40f434
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-07 12:30:42 -0700

    add AirBlock and DirtBlock

---
+++ src/com/sheaconlon/realcraft/blocks/AirBlock.java
@@ -0,0 +1,8 @@
+package com.sheaconlon.realcraft.blocks;
+
+/**
+ * A block of air.
+ */
+public class AirBlock extends Block {
+
+}
---
+++ src/com/sheaconlon/realcraft/blocks/DirtBlock.java
@@ -0,0 +1,8 @@
+package com.sheaconlon.realcraft.blocks;
+
+/**
+ * A block of dirt.
+ */
+public class DirtBlock extends Block {
+
+}
