ddf1761e3398585b275315dd3631c9057505d131
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-14 08:20:59 -0700

    use s instead of ns for Effect#tick

--- src/com/sheaconlon/realcraft/physics/Effect.java
+++ src/com/sheaconlon/realcraft/physics/Effect.java
@@ -10,14 +10,14 @@ public abstract class Effect {
     /**
      * Apply this effect to an entity.
      * @param entity The entity.
-     * @param time The number of nanoseconds to simulate the passage of.
+     * @param time The number of seconds to simulate the passage of.
      */
     public abstract void tick(final Entity entity, final double time);
 
     /**
      * Apply this effect to a block.
      * @param block The block.
-     * @param time The number of nanoseconds to simulate the passage of.
+     * @param time The number of seconds to simulate the passage of.
      */
     public abstract void tick(final Block block, final double time);
 }
