b66662db052138a5fc65fa527f6eaf547e24f796
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-14 08:19:51 -0700

    add Effect

---
+++ src/com/sheaconlon/realcraft/physics/Effect.java
@@ -0,0 +1,23 @@
+package com.sheaconlon.realcraft.physics;
+
+import com.sheaconlon.realcraft.blocks.Block;
+import com.sheaconlon.realcraft.entity.Entity;
+
+/**
+ * A physics effect.
+ */
+public abstract class Effect {
+    /**
+     * Apply this effect to an entity.
+     * @param entity The entity.
+     * @param time The number of nanoseconds to simulate the passage of.
+     */
+    public abstract void tick(final Entity entity, final double time);
+
+    /**
+     * Apply this effect to a block.
+     * @param block The block.
+     * @param time The number of nanoseconds to simulate the passage of.
+     */
+    public abstract void tick(final Block block, final double time);
+}
