b9711d24a29548c8ea24d68051710af9c60329c8
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-14 09:47:39 -0700

    overhaul Player

--- src/com/sheaconlon/realcraft/entity/entities/Player.java
+++ src/com/sheaconlon/realcraft/entity/entities/Player.java
@@ -3,13 +3,14 @@ package com.sheaconlon.realcraft.entity.entities;
 import com.sheaconlon.realcraft.entity.CompositeEntity;
 import com.sheaconlon.realcraft.entity.Entity;
 import com.sheaconlon.realcraft.physics.BoundingBox;
+import com.sheaconlon.realcraft.physics.Physical;
 import com.sheaconlon.realcraft.positioning.Position;
 import com.sheaconlon.realcraft.renderer.Quad;
 
 import java.util.LinkedList;
 
 /**
- * The player.
+ * The player, the avatar of the user.
  */
 public class Player extends CompositeEntity {
     /**
@@ -23,65 +24,21 @@ public class Player extends CompositeEntity {
     private static final BoundingBox BODY_BOUNDING_BOX = new BoundingBox(1,2, 1);
 
     /**
-     * The bounding box of a player.
-     */
-    private static final BoundingBox BOUNDING_BOX = new BoundingBox(1, 2, 1);
-
-    /**
-     * The player's head.
+     * A player's head.
      */
     private class PlayerHead extends Entity {
-        private final BoundingBox BOUNDING_BOX = new BoundingBox(1, 1, 1);
-
         /**
-         * Construct a player head.
-         * @param pos See {@link Entity#pos}.
-         * @param xAngle See {@link Entity#xAngle}.
-         * @param yAngle See {@link Entity#xAngle}.
-         * @param zAngle See {@link Entity#xAngle}.
+         * @see Physical#Physical(Position)
          */
-        PlayerHead(final Position pos, final double xAngle, final double yAngle, final double zAngle) {
-            super(pos, xAngle, yAngle, zAngle);
+        PlayerHead(final Position position) {
+            super(position);
         }
 
         /**
          * {@inheritDoc}
          */
         public Iterable<Quad> getQuads() {
-            return new LinkedList<>();
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public BoundingBox getBoundingBox() {
-            return Player.BODY_BOUNDING_BOX;
-        }
-    }
-
-    /**
-     * The player's body.
-     */
-    private class PlayerBody extends Entity {
-        private final BoundingBox BOUNDING_BOX = new BoundingBox(1,2, 1);
-
-        /**
-         * Construct a player body.
-         * @param pos See {@link Entity#pos}.
-         * @param xAngle See {@link Entity#xAngle}.
-         * @param yAngle See {@link Entity#xAngle}.
-         * @param zAngle See {@link Entity#xAngle}.
-         */
-        PlayerBody(final Position pos,
-                   final double xAngle, final double yAngle, final double zAngle) {
-            super(pos, xAngle, yAngle, zAngle);
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        public Iterable<Quad> getQuads() {
+            // TODO: Add quads for PlayerHead.
             return new LinkedList<>();
         }
 
@@ -95,9 +52,30 @@ public class Player extends CompositeEntity {
     }
 
     /**
-     * The differences between the y-coordinates of the anchor points of the player's head and the player's body.
+     * A player's body.
      */
-    private static final double HEAD_DELTA_Y = 1.25;
+    private class PlayerBody extends Entity {
+        /**
+         * {@inheritDoc}
+         */
+        public Iterable<Quad> getQuads() {
+            // TODO: Add quads for PlayerBody.
+            return new LinkedList<>();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public BoundingBox getBoundingBox() {
+            return Player.BODY_BOUNDING_BOX;
+        }
+    }
+
+    /**
+     * The difference between the y-coordinates of the player's head and the player's body, in feet.
+     */
+    private static final double HEAD_HEIGHT = 1.25;
 
     /**
      * The player's head.
@@ -110,23 +88,15 @@ public class Player extends CompositeEntity {
     private final PlayerBody body;
 
     /**
-     * Construct a player.
-     * @param pos See {@link Entity#pos}.
-     * @param xAngle See {@link Entity#xAngle}.
-     * @param yAngle See {@link Entity#xAngle}.
-     * @param zAngle See {@link Entity#xAngle}.
-     * @param headXAngle The {@code xAngle} of the player's head relative to the {@code xAngle} of the player.
-     * @param headYAngle The {@code yAngle} of the player's head relative to the {@code yAngle} of the player.
-     * @param headZAngle The {@code zAngle} of the player's head relative to the {@code zAngle} of the player.
+     * @see Physical#Physical{Position)
      */
-    public Player(final Position pos, final double xAngle, final double yAngle, final double zAngle,
-                  final double headXAngle, final double headYAngle, final double headZAngle) {
-        super(pos, xAngle, yAngle, zAngle);
-        this.body = new PlayerBody(pos, xAngle, yAngle, zAngle);
+    public Player(final Position position) {
+        super(position);
+        this.body = new PlayerBody();
         this.addConstituent(this.body);
-        pos.changeY(Player.HEAD_DELTA_Y);
-        this.head = new PlayerHead(pos, xAngle + headXAngle, yAngle + headYAngle,
-                zAngle + headZAngle);
+        final Position headPosition = new Position(position);
+        headPosition.changeY(Player.HEAD_HEIGHT);
+        this.head = new PlayerHead(headPosition);
         this.addConstituent(this.head);
     }
 }
