9911a517340b8d55870d22c4dafc6775c6748149
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-14 11:14:22 -0700

    overhaul ChunkGenerator, add #getPlayer(...)

--- src/com/sheaconlon/realcraft/world/ChunkGenerator.java
+++ src/com/sheaconlon/realcraft/world/ChunkGenerator.java
@@ -3,42 +3,85 @@ package com.sheaconlon.realcraft.world;
 import com.sheaconlon.realcraft.blocks.AirBlock;
 import com.sheaconlon.realcraft.blocks.Block;
 import com.sheaconlon.realcraft.blocks.DirtBlock;
+import com.sheaconlon.realcraft.entity.entities.Player;
 import com.sheaconlon.realcraft.positioning.BlockPosition;
 import com.sheaconlon.realcraft.positioning.ChunkPosition;
+import com.sheaconlon.realcraft.positioning.Position;
 
 /**
- * A chunk generator. Creates the initial state of the world.
+ * A chunk generator, which generates the initial state of chunks on demand.
  */
 public class ChunkGenerator {
+    /**
+     * The initial position of the player.
+     */
+    public static final Position PLAYER_INITIAL_POSITION = new Position(0, 0, 0);
+
     /**
      * The y-coordinate of the top of the ground.
      */
     private static final int GROUND_LEVEL = Chunk.SIZE / 2;
 
     /**
+     * The player of the world being generated.
+     */
+    private final Player player;
+
+    /**
+     * Construct a chunk generator.
+     */
+    public ChunkGenerator() {
+        this.player = new Player(ChunkGenerator.PLAYER_INITIAL_POSITION);
+    }
+
+    /**
+     * Get the player of the world being generated.
+     * @return The player of the world being generated.
+     */
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    /**
      * Generate the chunk at some position.
      *
-     * @param chunkPos The position.
-     * @return The chunk at {@code pos}.
+     * The world will consist of many layers of dirt blocks at even hash code block positions, then infinitely many
+     * air blocks.
+     * @param position The position.
+     * @return The chunk at {@code position}.
      */
-    Chunk getChunk(final ChunkPosition chunkPos) {
-        final Chunk chunk = new Chunk(chunkPos);
-        for (long bx = chunkPos.getX(); bx < chunkPos.getX() + Chunk.SIZE; bx++) {
-            for (long bz = chunkPos.getZ(); bz < chunkPos.getZ() + Chunk.SIZE; bz++) {
-                for (long by = chunkPos.getY(); by < chunkPos.getY() + Chunk.SIZE; by++) {
-                    final BlockPosition blockPos = new BlockPosition(bx, by, bz);
+    Chunk getChunk(final ChunkPosition position) {
+        final Chunk chunk = new Chunk(position);
+        for (long x = 0; x < Chunk.SIZE; x++) {
+            for (long z = 0; z < Chunk.SIZE; z++) {
+                for (long y = 0; y < Chunk.SIZE; y++) {
+                    final BlockPosition blockPosition = new BlockPosition(x + position.getX(),
+                            y + position.getY(), z + position.getZ());
                     Block block;
-                    // Place dirt up to blockY = GROUND_LEVEL - 1 so that the highest dirt blocks extend to
-                    // GROUND_LEVEL. For blockY <= GROUND_LEVEL - 1, place dirt blocks at every "even" position.
-                    if (blockPos.getY() <= GROUND_LEVEL - 1 && blockPos.hashCode() % 2 == 0) {
-                        block = new DirtBlock(blockPos);
+                    // Place dirt at every even hash code block position. The dirt blocks should be placed at
+                    // positions up to GROUND_LEVEL - 1 so that the highest dirt blocks' upper surfaces are at
+                    // GROUND_LEVEL.
+                    if (blockPosition.getY() <= GROUND_LEVEL - 1 && blockPosition.hashCode() % 2 == 0) {
+                        block = new DirtBlock(blockPosition);
                     } else {
-                        block = new AirBlock(blockPos);
+                        block = new AirBlock(blockPosition);
                     }
-                    chunk.setBlock(blockPos, block);
+                    chunk.setBlock(blockPosition, block);
                 }
             }
         }
+        if (position.equals(ChunkGenerator.PLAYER_INITIAL_POSITION.toChunkPosition())) {
+            this.placePlayer(chunk);
+        }
         return chunk;
+    }
+
+    /**
+     * Place the player at {@link ChunkGenerator#PLAYER_INITIAL_POSITION} in a chunk.
+     * @param chunk The chunk.
+     */
+    private void placePlayer(final Chunk chunk) {
+        // TODO: Choose a randomized, safe place instead of a preset, possibly dangerous place.
+        chunk.addEntity(this.player);
     }
 }
