dab6cea4bbfc98cdf1e88f46795cdfb36b66fb68
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-23 18:50:01 -0700

    VBORenderer: fix #sendVBOData()

--- src/com/sheaconlon/realcraft/renderer/VBORenderer.java
+++ src/com/sheaconlon/realcraft/renderer/VBORenderer.java
@@ -57,7 +57,7 @@ public abstract class VBORenderer<T> extends Renderer2<T> {
      */
     protected void sendVBOData(final FloatBuffer data) {
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboHandle);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, data, GL15.GL_STATIC_DRAW);
         this.vboLength = data.remaining() / (3 + 3 + 3);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, data, GL15.GL_STATIC_DRAW);
     }
 }
