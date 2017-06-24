8953a93fb0b833e6a85f7f0be7db12488cba9ee3
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-23 19:27:12 -0700

    VBORenderer: refactor

--- src/com/sheaconlon/realcraft/renderer/ChunkRenderer.java
+++ src/com/sheaconlon/realcraft/renderer/ChunkRenderer.java
@@ -1,44 +1,26 @@
 package com.sheaconlon.realcraft.renderer;
 
-import com.sheaconlon.realcraft.positioning.Position;
 import com.sheaconlon.realcraft.world.Chunk;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
 
-import java.nio.DoubleBuffer;
-import java.nio.FloatBuffer;
-
+/**
+ * A renderer of a chunk.
+ */
 public class ChunkRenderer extends VBORenderer<Chunk> {
-    private final Chunk chunk;
-
+    /**
+     * Create a renderer for some chunk.
+     *
+     * An OpenGL context must be current.
+     * @param chunk The chunk.
+     */
     public ChunkRenderer(final Chunk chunk) {
         super(chunk);
-        this.chunk = chunk;
-        // TODO: Make ChunkRenderer render entities as well as blocks.
-        // TODO: Make ChunkRenderer work for variable number of quads.
-        final int quadCount = Chunk.SIZE * Chunk.SIZE * Chunk.SIZE * 6;
-        final int doublesPerQuad = (3 + 3 + 3) * 4;
-        final FloatBuffer vboData = BufferUtils.createFloatBuffer(quadCount * doublesPerQuad);
-        for (final Quad quad : chunk) {
-            final float[] color = quad.getColor();
-            for (final Vertex vertex : quad.getVertices()) {
-                final Position position = vertex.getPosition();
-                vboData.put((float)position.getX());
-                vboData.put((float)position.getY());
-                vboData.put((float)position.getZ());
-                vboData.put(color[0]);
-                vboData.put(color[1]);
-                vboData.put(color[2]);
-                vboData.put(vertex.getNormal()[0]);
-                vboData.put(vertex.getNormal()[1]);
-                vboData.put(vertex.getNormal()[2]);
-            }
-        }
-        vboData.flip();
-        this.sendVBOData(vboData);
     }
 
+    /**
+     * Render the chunk.
+     * @param chunk The chunk.
+     */
     void render(final Chunk chunk) {
-        super.render();
+        this.render();
     }
 }
--- src/com/sheaconlon/realcraft/renderer/VBORenderer.java
+++ src/com/sheaconlon/realcraft/renderer/VBORenderer.java
@@ -1,26 +1,70 @@
 package com.sheaconlon.realcraft.renderer;
 
+import com.sheaconlon.realcraft.positioning.Position;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 
-import java.nio.DoubleBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A thing which knows how to render some thing using VBOs.
+ * A renderer of some {@link Iterable<Quad>} thing which uses VBOs.
  */
-public abstract class VBORenderer<T> extends Renderer2<T> {
+public abstract class VBORenderer<T extends Iterable<Quad>> extends Renderer2<T> {
+    /**
+     * The number of floats used to specify a vertex's position.
+     */
+    private static final int FLOATS_PER_POSITION = 3;
+
+    /**
+     * The number of floats used to specify a quad's color.
+     */
+    private static final int FLOATS_PER_COLOR = 3;
+
+    /**
+     * The number of floats used to specify a vertex's normal.
+     */
+    private static final int FLOATS_PER_NORMAL = 3;
+
+    /**
+     * The number of bytes in a float.
+     */
+    private static final int BYTES_PER_FLOAT = Float.SIZE / Byte.SIZE;
+
+    /**
+     * The total number of floats used to specify a vertex.
+     */
+    private static final int FLOATS_PER_VERTEX = FLOATS_PER_POSITION + FLOATS_PER_COLOR + FLOATS_PER_NORMAL;
+
+    /**
+     * The total number of bytes used to specify a vertex.
+     */
+    private static final int BYTES_PER_VERTEX = FLOATS_PER_VERTEX * BYTES_PER_FLOAT;
+
+    /**
+     * Allocate a new VBO and return its OpenGL handle.
+     *
+     * An OpenGL context must be current.
+     * @return The OpenGL handle of a new VBO.
+     */
+    private static int allocateVBO() {
+        final IntBuffer vboHandleBuffer = BufferUtils.createIntBuffer(1);
+        GL15.glGenBuffers(vboHandleBuffer);
+        return vboHandleBuffer.get();
+    }
+
     /**
      * The OpenGL handle for this VBO renderer's VBO.
      */
     private final int vboHandle;
 
     /**
-     * The number of elements in this renderer's VBO.
+     * The number of vertices in this VBO renderer's VBO.
      */
-    private int vboLength;
+    private int numVertices;
 
     /**
      * Create a VBO renderer for some thing.
@@ -30,34 +74,69 @@ public abstract class VBORenderer<T> extends Renderer2<T> {
      */
     protected VBORenderer(final T thing) {
         super(thing);
-        final IntBuffer vboHandleBuffer = BufferUtils.createIntBuffer(1);
-        GL15.glGenBuffers(vboHandleBuffer);
-        this.vboHandle = vboHandleBuffer.get();
+        this.vboHandle = VBORenderer.allocateVBO();
+        final FloatBuffer dataBuffer = this.createBuffer(thing);
+        this.writeData(dataBuffer);
     }
 
     /**
-     * Render the previously sent VBO data.
+     * Create a buffer holding the vertex data of some thing and set this VBO renderer's {@link #numVertices}
+     * appropriately.
+     * @param thing The thing.
+     * @return A buffer holding the vertex data of the thing.
+     */
+    private FloatBuffer createBuffer(final Iterable<Quad> thing) {
+        final List<Float> data = new ArrayList<>();
+        this.numVertices = 0;
+        for (final Quad quad : thing) {
+            final float[] color = quad.getColor();
+            for (final Vertex vertex : quad.getVertices()) {
+                this.numVertices++;
+                final Position position = vertex.getPosition();
+                data.add((float)position.getX());
+                data.add((float)position.getY());
+                data.add((float)position.getZ());
+                data.add(color[0]);
+                data.add(color[1]);
+                data.add(color[2]);
+                final float[] normal = vertex.getNormal();
+                data.add(normal[0]);
+                data.add(normal[1]);
+                data.add(normal[2]);
+            }
+        }
+        final FloatBuffer buffer = BufferUtils.createFloatBuffer(data.size());
+        for (final float entry : data) {
+            buffer.put(entry);
+        }
+        buffer.flip();
+        return buffer;
+    }
+
+    /**
+     * Write a buffer to this VBO renderer's VBO.
      *
-     * Must be called after {@link #sendVBOData(FloatBuffer)}. An OpenGL context must be current.
+     * An OpenGL context must be current.
+     * @param buffer The buffer.
+     */
+    private void writeData(final FloatBuffer buffer) {
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboHandle);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+    }
+
+    /**
+     * Render the thing.
+     *
+     * An OpenGL context must be current.
      */
     @Override
     void render() {
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboHandle);
-        GL11.glVertexPointer(3, GL11.GL_FLOAT, (3 * 4) + (3 * 4) + (3 * 4), 0);
-        GL11.glColorPointer(3, GL11.GL_FLOAT, (3 * 4) + (3 * 4) + (3 * 4), (3 * 4));
-        GL11.glNormalPointer(GL11.GL_FLOAT, (3 * 4) + (3 * 4) + (3 * 4), (3 * 4) + (3 * 4));
-        GL11.glDrawArrays(GL11.GL_QUADS, 0, this.vboLength);
-    }
-
-    /**
-     * Send VBO data to the GPU.
-     *
-     * An OpenGL context must be current.
-     * @param data The data.
-     */
-    protected void sendVBOData(final FloatBuffer data) {
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboHandle);
-        this.vboLength = data.remaining() / (3 + 3 + 3);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, data, GL15.GL_STATIC_DRAW);
+        GL11.glVertexPointer(VBORenderer.FLOATS_PER_POSITION, GL11.GL_FLOAT, VBORenderer.BYTES_PER_VERTEX, 0);
+        GL11.glColorPointer(VBORenderer.FLOATS_PER_COLOR, GL11.GL_FLOAT, VBORenderer.BYTES_PER_VERTEX,
+                VBORenderer.FLOATS_PER_POSITION * VBORenderer.BYTES_PER_FLOAT);
+        GL11.glNormalPointer(GL11.GL_FLOAT, VBORenderer.BYTES_PER_VERTEX,
+                (VBORenderer.FLOATS_PER_POSITION + VBORenderer.FLOATS_PER_COLOR) * VBORenderer.BYTES_PER_FLOAT);
+        GL11.glDrawArrays(GL11.GL_QUADS, 0, this.numVertices);
     }
 }
