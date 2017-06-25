7c7d91873c1a8dc043474820ee7662a95af87d4a
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-25 10:50:59 -0700

    add Container and WorldObject

---
+++ src/com/sheaconlon/realcraft/world/Container.java
@@ -0,0 +1,31 @@
+package com.sheaconlon.realcraft.world;
+
+import java.util.Iterator;
+
+/**
+ * A container of world objects.
+ */
+public abstract class Container {
+    /**
+     * The container of this container.
+     */
+    private final Container container;
+
+    /**
+     * The position of the anchor point of this container, relative to the position of the anchor point of this
+     * container's container.
+     */
+    private final double[] position;
+
+    /**
+     * Create a container.
+     * @param container The container of this container.
+     * @param position The initial position of the anchor point of the container.
+     */
+    public Container(final Container container, final double[] position) {
+        this.container = container;
+        this.position = position;
+    }
+
+    public abstract Iterator<WorldObject> getContents();
+}
---
+++ src/com/sheaconlon/realcraft/world/WorldObject.java
@@ -0,0 +1,47 @@
+package com.sheaconlon.realcraft.world;
+
+/**
+ * A world object.
+ */
+public abstract class WorldObject {
+    /**
+     * The container of this world object.
+     */
+    private final Container container;
+
+    /**
+     * The position of the anchor point of this world object, relative to the position of the anchor point of
+     * this world object's container.
+     */
+    private final double[] position;
+
+    /**
+     * The velocity of this world object.
+     */
+    private final double[] velocity;
+
+    /**
+     * Create a world object.
+     * @param container The container of this world object.
+     * @param position The initial position of the anchor point of the world object.
+     * @param velocity The initial velocity of the world object.
+     */
+    public WorldObject(final Container container, final double[] position, final double[] velocity) {
+        this.container = container;
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    /**
+     * Get the vertex data of this world object, with positions relative to this world object's anchor.
+     *
+     * The vertex data of one vertex consists of an array of 3 float arrays. The first float array consists
+     * of three floats which are the x-, y-, and z-coordinates of the position of the vertex. The second float
+     * array consists of three floats which are the red, green, and blue components of the color of the vertex.
+     * The third float array consists of three floats which are the x-, y-, and z-coordinates of the normal
+     * vector of the vertex. The vertex data of this world object consists of the array of the vertex data of all
+     * its vertices, in some arbitrary, consistent order.
+     * @return The vertex data of this world object, with positions relative to this world object's anchor.
+     */
+    public abstract float[][][] getVertexData();
+}
