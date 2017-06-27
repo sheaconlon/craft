1cb463bfb2b2f8f2541319ae09087dfa6c199511
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-27 15:08:58 -0700

    PositionUtilities: fix toChunkPosition()

--- src/com/sheaconlon/realcraft/utilities/PositionUtilities.java
+++ src/com/sheaconlon/realcraft/utilities/PositionUtilities.java
@@ -10,11 +10,22 @@ public class PositionUtilities {
      * Get the position of the anchor point of the chunk containing some position.
      * @return The position of the anchor point of the chunk containing some position.
      */
-    public static int[] toChunkPosition(final double[] position) {
-       final int[] chunkPosition = new int[position.length];
-       for (int i = 0; i < position.length; i++) {
-           chunkPosition[i] = (int)Math.floor(position[i] / Chunk.SIZE);
+    public static int[] toChunkPosition(final double[] pos) {
+       final int[] chunkPos = new int[3];
+       for (int i = 0; i < 3; i++) {
+           chunkPos[i] = Chunk.SIZE * PositionUtilities.floorCoordinate(pos[i] / Chunk.SIZE);
        }
-       return chunkPosition;
+       return chunkPos;
+    }
+
+    /**
+     * Floor a coordinate.
+     *
+     * The floor of a coordinate is the greatest integer which is less than or equal to the coordinate.
+     * @param coord The coordinate.
+     * @return The floor of the coordinate.
+     */
+    private static int floorCoordinate(final double coord) {
+        return (int)Math.floor(coord);
     }
 }
