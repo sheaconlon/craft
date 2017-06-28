695e36f475135839e2e7e587fa8e17b0f8c6f9fe
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-06-28 10:01:41 -0700

    Launcher: add Prerenderer

--- src/com/sheaconlon/realcraft/Launcher.java
+++ src/com/sheaconlon/realcraft/Launcher.java
@@ -1,6 +1,7 @@
 package com.sheaconlon.realcraft;
 
 import com.sheaconlon.realcraft.generator.Generator;
+import com.sheaconlon.realcraft.renderer.Prerenderer;
 import com.sheaconlon.realcraft.renderer.Renderer;
 import com.sheaconlon.realcraft.ui.UserInterface;
 import com.sheaconlon.realcraft.world.World;
@@ -10,12 +11,16 @@ public class Launcher {
         final World world = new World();
         final UserInterface ui = new UserInterface(world);
         ui.show();
-        final Thread rendererThread = new Thread(new Renderer(world, ui.getWindowHandle(), ui.getDimensions()));
+        final Renderer renderer = new Renderer(world, ui.getWindowHandle(), ui.getDimensions());
+        final Thread rendererThread = new Thread(renderer);
         final Thread generatorThread = new Thread(new Generator(world));
+        final Thread prerendererThread = new Thread(new Prerenderer(world, renderer));
         generatorThread.start();
         rendererThread.start();
+        prerendererThread.start();
         ui.run();
         rendererThread.interrupt();
         generatorThread.interrupt();
+        prerendererThread.interrupt();
     }
 }
