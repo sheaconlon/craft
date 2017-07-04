8f76f391540a3c56f288f44745973f0a97f35fa0
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-03 20:48:04 -0700

    PositionUtilities: prioritize nearby chunks

--- src/com/sheaconlon/realcraft/utilities/PositionUtilities.java
+++ src/com/sheaconlon/realcraft/utilities/PositionUtilities.java
@@ -2,6 +2,8 @@ package com.sheaconlon.realcraft.utilities;
 
 import com.sheaconlon.realcraft.world.Chunk;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -33,13 +35,53 @@ public class PositionUtilities {
     }
 
     /**
-     * Get the positions of the anchor points of the chunks which are "nearby" some chunk of interest.
+     * A comparator of chunk positions based on their weighted taxicab distance from a chunk of interest.
+     */
+    private static class ChunkPositionComparator implements Comparator<int[]> {
+        /**
+         * Weights for the distance metric used by a chunk position comparator.
+         */
+        private static final double[] WEIGHTS = new double[]{1, 2, 1};
+
+        /**
+         * The position of the anchor point of the chunk of interest.
+         */
+        private final int[] pos;
+
+        /**
+         * Create a chunk position comparator.
+         * @param pos See {@link #pos}.
+         */
+        ChunkPositionComparator(final int[] pos) {
+            this.pos = pos;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int compare(int[] a, int[] b) {
+            final int[] diffA = ArrayUtilities.subtract(a, this.pos);
+            final int[] absDiffA = ArrayUtilities.abs(diffA);
+            final double[] weightedDiffA = ArrayUtilities.multiply(absDiffA, ChunkPositionComparator.WEIGHTS);
+            final double distA = ArrayUtilities.sum(weightedDiffA);
+            final int[] diffB = ArrayUtilities.subtract(b, this.pos);
+            final int[] absDiffB = ArrayUtilities.abs(diffB);
+            final double[] weightedDiffB = ArrayUtilities.multiply(absDiffB, ChunkPositionComparator.WEIGHTS);
+            final double distB = ArrayUtilities.sum(weightedDiffB);
+            return Double.compare(distA, distB);
+        }
+    }
+
+    /**
+     * Get the positions of the anchor points of the chunks which are "nearby" some chunk of interest, in ascending
+     * order of some distance metric.
      * @param pos The position of the anchor point of the chunk of interest.
      * @param chunkRadius The number of chunks in each direction that should be considered "nearby" a chunk.
      * @return The positions of the anchor points of the chunks which are "nearby" some chunk of interest.
      */
     public static Iterable<int[]> getNearbyChunkPositions(final int[] pos, final int chunkRadius) {
-        final List<int[]> positions = new LinkedList<>();
+        final List<int[]> positions = new ArrayList<>();
         for (int x = pos[0] - chunkRadius * Chunk.SIZE; x <= pos[0] + chunkRadius * Chunk.SIZE; x += Chunk.SIZE) {
             for (int y = pos[1] - chunkRadius * Chunk.SIZE; y <= pos[1] + chunkRadius * Chunk.SIZE; y += Chunk.SIZE) {
                 for (int z = pos[2] - chunkRadius * Chunk.SIZE; z <= pos[2] + chunkRadius * Chunk.SIZE; z += Chunk.SIZE) {
@@ -47,6 +89,7 @@ public class PositionUtilities {
                 }
             }
         }
+        positions.sort(new PositionUtilities.ChunkPositionComparator(pos));
         return positions;
     }
 }
