8d3afe726bef8cc1bd41a6a4bfbb43efb2cb2993
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-06 13:42:04 -0700

    assign Player to a Chunk in World

--- src/com/sheaconlon/realcraft/world/World.java
+++ src/com/sheaconlon/realcraft/world/World.java
@@ -140,6 +140,11 @@ public class World {
      */
     public void loadChunk(final int[] pos, final Chunk chunk) {
         final List<Integer> posList = ArrayUtilities.toList(pos);
+        final List<Integer> playerChunkPosList =
+                ArrayUtilities.toList(PositionUtilities.toChunkPosition(this.player.getPosition()));
+        if (posList.equals(playerChunkPosList)) {
+            chunk.addEntity(this.player);
+        }
         this.chunks.put(posList, chunk);
     }
 
