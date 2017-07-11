bccbe39260f6c8ccaf7c29a997d93a867c937674
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-11 14:12:54 -0700

    add TaskRunner to run tasks

---
+++ src/com/sheaconlon/realcraft/concurrency/TaskRunner.java
@@ -0,0 +1,33 @@
+package com.sheaconlon.realcraft.concurrency;
+
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+/**
+ * A runner of tasks.
+ */
+public class TaskRunner implements Runnable {
+    private static final Queue<Task> queue = new PriorityBlockingQueue<>();
+
+    /**
+     * Run tasks, high-priority ones first.
+     */
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            final Task topTask = queue.poll();
+            topTask.run();
+            if (!topTask.done()) {
+                TaskRunner.enqueueTask(topTask);
+            }
+        }
+    }
+
+    /**
+     * Enqueue a task for running by any task runner.
+     * @param task The task.
+     */
+    public static void enqueueTask(final Task task) {
+        TaskRunner.queue.add(task);
+    }
+}
