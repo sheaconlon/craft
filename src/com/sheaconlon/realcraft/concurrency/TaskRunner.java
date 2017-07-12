d6231be0e8138730b0b4f7802bdd274c3bcc364f
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-11 18:04:19 -0700

    make queue of TaskRunner non-static and supplied in constructor

--- src/com/sheaconlon/realcraft/concurrency/TaskRunner.java
+++ src/com/sheaconlon/realcraft/concurrency/TaskRunner.java
@@ -1,13 +1,20 @@
 package com.sheaconlon.realcraft.concurrency;
 
 import java.util.Queue;
-import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * A runner of tasks.
  */
 public class TaskRunner implements Runnable {
-    private static final Queue<Task> queue = new PriorityBlockingQueue<>();
+    private final Queue<Worker> workerQueue;
+
+    /**
+     * Create a task runner.
+     * @param workerQueue The queue of workers which this task runner should draw from.
+     */
+    public TaskRunner(final Queue<Worker> workerQueue) {
+        this.workerQueue = workerQueue;
+    }
 
     /**
      * Run tasks, high-priority ones first.
@@ -15,19 +22,11 @@ public class TaskRunner implements Runnable {
     @Override
     public void run() {
         while (!Thread.interrupted()) {
-            final Worker topTask = queue.poll();
+            final Worker topTask = this.workerQueue.poll();
             topTask.run();
             if (!topTask.done()) {
-                TaskRunner.enqueueTask(topTask);
+                this.workerQueue.add(topTask);
             }
         }
-    }
-
-    /**
-     * Enqueue a task for running by any task runner.
-     * @param task The task.
-     */
-    public static void enqueueTask(final Task task) {
-        TaskRunner.queue.add(task);
     }
 }
