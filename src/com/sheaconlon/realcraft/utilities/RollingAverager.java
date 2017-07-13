9bae047ac6965fb66253971cfa1c1d9ae3a905ae
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-12 17:55:38 -0700

    add RollingAverager utility

---
+++ src/com/sheaconlon/realcraft/utilities/RollingAverager.java
@@ -0,0 +1,39 @@
+package com.sheaconlon.realcraft.utilities;
+
+/**
+ * Something which takes a rolling average.
+ */
+public class RollingAverager {
+    private final int samples;
+
+    private RingBuffer<Double> buffer;
+
+    private double sum;
+
+    /**
+     * Create a rolling averager.
+     * @param samples The number of samples that it should average over.
+     */
+    public RollingAverager(final int samples) {
+        this.samples = samples;
+        this.buffer = new RingBuffer<>(this.samples);
+        this.sum = 0;
+    }
+
+    /**
+     * Add a sample.
+     * @param x The sample.
+     */
+    public void add(final double x) {
+        final double old = this.buffer.addFront(x);
+        this.sum -= old;
+        this.sum += x;
+    }
+
+    /**
+     * @return The rolling average of the samples.
+     */
+    public double average() {
+        return this.sum / this.buffer.size();
+    }
+}
