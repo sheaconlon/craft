32265231338ee102cb293611a12f364e25b2c3f5
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-13 13:52:12 -0700

    add assigned workers to runner

--- src/com/sheaconlon/realcraft/concurrency/Runner.java
+++ src/com/sheaconlon/realcraft/concurrency/Runner.java
@@ -1,30 +1,48 @@
 package com.sheaconlon.realcraft.concurrency;
 
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
  * A runner, which ticks workers.
  */
 public class Runner implements Runnable {
-    private final Queue<Worker> workerQueue;
+    private final Queue<Worker> sharedWorkers;
+
+    private final Queue<Worker> assignedWorkers;
 
     /**
      * Create a runner.
-     * @param workerQueue The queue of workers which this runner should draw from.
+     * @param sharedWorkers Workers which this runner should tick. This queue may be shared with other runners.
      */
-    public Runner(final Queue<Worker> workerQueue) {
-        this.workerQueue = workerQueue;
+    public Runner(final Queue<Worker> sharedWorkers) {
+        this.sharedWorkers = sharedWorkers;
+        this.assignedWorkers = new PriorityQueue<>();
     }
 
     /**
-     * Tick workers, following the order of the queue.
+     * Tick workers, preferring ones that are "small" according to their natural ordering.
      */
     @Override
     public void run() {
         while (!Thread.interrupted()) {
-            final Worker topTask = this.workerQueue.poll();
-            topTask.tick();
-            this.workerQueue.add(topTask);
+            final Worker topSharedWorker = this.sharedWorkers.poll();
+            final Worker topAssignedWorker = this.assignedWorkers.poll();
+            if (topSharedWorker.compareTo(topAssignedWorker) < 0) { // Ties go to assigned workers.
+                topSharedWorker.tick();
+                this.sharedWorkers.add(topSharedWorker);
+            } else {
+                topAssignedWorker.tick();
+                this.assignedWorkers.add(topAssignedWorker);
+            }
         }
+    }
+
+    /**
+     * Assign a worker for this runner to tick. This worker is not ticked by other runners.
+     * @param w The worker.
+     */
+    public void assign(final Worker w) {
+        this.assignedWorkers.add(w);
     }
 }
