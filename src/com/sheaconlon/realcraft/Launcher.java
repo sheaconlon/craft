8846df3b35bd7b7dcc8e8e3f978ea58c6ac90438
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-07-17 08:45:59 -0700

    add Simulator to Launcher

--- src/com/sheaconlon/realcraft/Launcher.java
+++ src/com/sheaconlon/realcraft/Launcher.java
@@ -5,6 +5,7 @@ import com.sheaconlon.realcraft.concurrency.Worker;
 import com.sheaconlon.realcraft.generator.Generator;
 import com.sheaconlon.realcraft.renderer.Prerenderer;
 import com.sheaconlon.realcraft.renderer.Renderer;
+import com.sheaconlon.realcraft.simulator.Simulator;
 import com.sheaconlon.realcraft.ui.UserInterface;
 import com.sheaconlon.realcraft.world.World;
 
@@ -19,11 +20,13 @@ public class Launcher {
         final Renderer renderer = new Renderer(world, ui.getWindowHandle(), ui.getDimensions());
         final Generator generator = new Generator(world);
         final Prerenderer prerenderer = new Prerenderer(world, renderer);
+        final Simulator simulator = new Simulator(world);
         final Worker[] workers = new Worker[]{
                 ui,
                 renderer,
                 generator,
-                prerenderer
+                prerenderer,
+                simulator
         };
 
         final Queue<Worker> sharedWorkers = new PriorityBlockingQueue<>();
