2457d638d1a6433f813c9051fd261462dfb9b451
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-08-04 14:47:25 -0700

    refactor Player; make singleton

--- src/com/sheaconlon/realcraft/entities/Player.java
+++ src/com/sheaconlon/realcraft/entities/Player.java
@@ -8,51 +8,50 @@ import com.sheaconlon.realcraft.world.WorldObject;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * The player, the avatar of the user.
  */
 public class Player extends Animal {
-    private static final double COMPRESSIVE_STRENGTH = 100;
-    private static final double MASS = 100;
-    private static final Vector[] HITBOX_POSITIONS = new Vector[]{
-            new Vector(-0.3, 0, -0.3)
-    };
-    private static final Vector[] HITBOX_DIMS = new Vector[]{
-            new Vector(0.6, 1.75, 0.6)
-    };
-
+    // ##### PUBLIC STATIC FINAL #####
     /**
-     * The initial velocity of a player.
+     * The player.
      */
-    private static final Vector INITIAL_VELOCITY = new Vector(0, 0, 0);
+    public static final Player PLAYER = new Player();
 
-    private final List<Hitbox> hitboxes;
+    // ##### PRIVATE STATIC FINAL #####
+    private static final Vector INITIAL_POSITION = Vector.ZERO;
+    private static final double INITIAL_ORIENTATION = 0;
+    private static final Vector INITIAL_VELOCITY = Vector.ZERO;
+    private static final double INITIAL_VERTICAL_ORIENTATION = 0;
+    private static final List<Hitbox> HITBOXES = Collections.unmodifiableList(
+            Stream.of(
+                new Hitbox(
+                        null,
+                        new Vector(-0.3, 0, -0.3),
+                        new Vector(0.6, 1.75, 0.6)
+                )
+            ).collect(
+                    Collectors.toList()
+            )
+    );
+    private static final List<Vertex> VERTICES = Collections.unmodifiableList(Collections.emptyList());
 
-    /**
-     * Create a player.
-     * @param pos See {@link WorldObject#getPos()}.
-     * @param orient See {@link WorldObject#getOrient()}.
-     * @param vertOrient See {@link Animal#getVertOrient()}.
-     */
-    public Player(final Vector pos, final double orient, final double vertOrient) {
-        super(pos, Player.INITIAL_VELOCITY, orient, vertOrient);
-        this.hitboxes = new LinkedList<>();
-        for (int i = 0; i < HITBOX_POSITIONS.length; i++) {
-            this.hitboxes.add(new Hitbox(this, HITBOX_POSITIONS[i], HITBOX_DIMS[i]));
-        }
+    // ##### CONSTRUCTORS #####
+    private Player() {
+        super(INITIAL_POSITION, INITIAL_VELOCITY, INITIAL_ORIENTATION, INITIAL_VERTICAL_ORIENTATION);
     }
 
-    /**
-     * {@inheritDoc}
-     */
+    // ##### WORLD OBJECT OVERRIDES #####
     @Override
     public List<Vertex> getVertices() {
-        return Collections.emptyList();
+        return VERTICES;
     }
 
     @Override
     public List<Hitbox> getHitboxes() {
-        return Collections.unmodifiableList(this.hitboxes);
+        return HITBOXES;
     }
 }
