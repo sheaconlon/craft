a50388f10162f19d97e481e56ca0de17f02a67da
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-08-05 12:41:26 -0700

    draft generator

--- src/com/sheaconlon/realcraft/generator/Generator.java
+++ src/com/sheaconlon/realcraft/generator/Generator.java
@@ -1,153 +1,85 @@
 package com.sheaconlon.realcraft.generator;
 
-import com.sheaconlon.realcraft.blocks.Block;
 import com.sheaconlon.realcraft.blocks.DirtBlock;
-import com.sheaconlon.realcraft.blocks.WoodBlock;
-import com.sheaconlon.realcraft.renderer.Renderer;
+import com.sheaconlon.realcraft.concurrency.Worker;
+import com.sheaconlon.realcraft.entities.Player;
 import com.sheaconlon.realcraft.utilities.Vector;
 import com.sheaconlon.realcraft.world.Chunk;
-import com.sheaconlon.realcraft.world.World;
-import com.sheaconlon.realcraft.concurrency.Worker;
-import com.sheaconlon.realcraft.blocks.AirBlock;
-import com.sheaconlon.realcraft.blocks.LeafBlock;
 
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.List;
-import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Set;
 
-/**
- * A generator. Responsible for procedurally generating chunks of the world and loading them into the world just in time
- * for the player to view them.
- */
 public class Generator extends Worker {
-    private static final double TREE_GENERATION_PROBABILITY = (double)4 / (double)(10 * 10);
-    private static final int[] TREE_TRUNK_HEIGHTS = new int[]{4, 5, 5, 5, 6, 7};
-    private static final double[] TREE_LEAF_RADII = new double[]{2.5, 3, 3, 3, 4, 5};
+    // ##### PRIVATE STATIC FINAL #####
+    private static final PerlinNoiseGenerator TERRAIN_NOISE_GENERATOR =
+            new PerlinNoiseGenerator(1, 1, x -> x);
 
-    /**
-     * A generator's return value for {@link #getTargetFreq()}.
-     */
-    private static final double TARGET_FREQ = 4;
+    // ##### PRIVATE FINAL #####
+    private final Set<Chunk> generated;
 
-    /**
-     * The y-coordinate of the highest ground blocks.
-     */
-    public static final int GROUND_LEVEL = 45;
-
-    /**
-     * The number of extra chunks in each direction from the player's chunk to keep loaded.
-     */
-    private static final int LOAD_DISTANCE = Renderer.RENDER_DISTANCE;
-
-    /**
-     * The world which this generator must generate chunks for.
-     */
-    private final World world;
-
-    /**
-     * Create a generator.
-     *
-     * @param world See {@link #world}.
-     */
-    public Generator(final World world) {
-        this.world = world;
+    // ##### CONSTRUCTORS #####
+    public Generator() {
+        this.generated = new HashSet<>();
     }
+
+    // ##### PRIVATE STATIC FINAL #####
+    private static final int RADIUS = 1;
+
+    // ##### WORKER OVERRIDES #####
+    private static final PRIORITY_LEVEL PRIORITY_LEVEL = Worker.PRIORITY_LEVEL.MEDIUM;
 
     @Override
     public PRIORITY_LEVEL getPriorityLevel() {
-        return PRIORITY_LEVEL.MEDIUM;
+        return PRIORITY_LEVEL;
     }
+
+    private static final String NAME = "Generator";
 
     @Override
     public String toString() {
-        return "Generator";
+        return NAME;
     }
+
+    private static final boolean NEEDS_MAIN_THREAD = false;
 
     @Override
     public boolean needsMainThread() {
-        return false;
+        return NEEDS_MAIN_THREAD;
     }
+
+    private static final boolean NEEDS_DEDICATED_THREAD = false;
 
     @Override
     public boolean needsDedicatedThread() {
-        return false;
+        return NEEDS_DEDICATED_THREAD;
     }
+
+    private static final int CHUNKS_PER_TICK = 3;
+
+    @Override
+    protected void tick(double interval) {
+        final Chunk playerChunk = Chunk.containingChunk(Player.PLAYER.getPos());
+        for (final Chunk chunkNearPlayer : playerChunk.chunksNearby(RADIUS)) {
+            if (!this.generated.contains(chunkNearPlayer)) {
+                this.generate(chunkNearPlayer);
+                this.generated.add(chunkNearPlayer);
+            }
+        }
+    }
+
+    private static final double TARGET_FREQ = 4;
 
     @Override
     protected double getTargetFreq() {
-        return Generator.TARGET_FREQ;
+        return TARGET_FREQ;
     }
 
-    /**
-     * If necessary, generate any unloaded chunks near the player and load them into the world.
-     */
-    @Override
-    public void tick(final double elapsedTime) {
-        final Vector playerChunkPos = Chunk.toChunkPos(this.world.getPlayer().getPos());
-        int numberDone = 0;
-        for (final Vector chunkPos : Chunk.getChunkPosNearby(playerChunkPos, Generator.LOAD_DISTANCE)) {
-            if (!world.chunkLoaded(chunkPos)) {
-                world.loadChunk(chunkPos, this.generateChunk(chunkPos));
-                numberDone++;
-                if (numberDone == 3) {
-                    return;
-                }
-            }
-        }
-    }
-
-    private Chunk generateChunk(final Vector pos) {
-        final Chunk chunk = new Chunk(pos);
-        this.generateGround(pos, chunk);
-        this.generateTrees(pos, chunk);
-        return chunk;
-    }
-
-    private void generateGround(final Vector pos, final Chunk chunk) {
-        final double[] blockPos = new double[3];
-        for (blockPos[0] = pos.getX(); blockPos[0] < pos.getX() + Chunk.SIZE; blockPos[0]++) {
-            for (blockPos[1] = pos.getY(); blockPos[1] < pos.getY() + Chunk.SIZE
-                    && blockPos[1] < Generator.GROUND_LEVEL; blockPos[1]++) {
-                for (blockPos[2] = pos.getZ(); blockPos[2] < pos.getZ() + Chunk.SIZE; blockPos[2]++) {
-                    final Vector blockPosVec = new Vector(blockPos[0], blockPos[1], blockPos[2]);
-                    final Block block = new DirtBlock(blockPosVec);
-                    chunk.putBlock(blockPosVec, block);
-                }
-            }
-        }
-    }
-
-    private void generateTrees(final Vector pos, final Chunk chunk) {
-        if (pos.getY() > GROUND_LEVEL || pos.getY() + Chunk.SIZE < GROUND_LEVEL) {
-            return;
-        }
-        final Vector lo = Vector.setY(pos, GROUND_LEVEL);
-        final Vector hi = Vector.add(Vector.setY(pos, GROUND_LEVEL), new Vector(Chunk.SIZE - 1, 0,Chunk.SIZE - 1));
-        for (final Vector anchor : Vector.between(lo, hi)) {
-            if (ThreadLocalRandom.current().nextDouble() < TREE_GENERATION_PROBABILITY) {
-                this.generateTree(pos, anchor, chunk);
-            }
-        }
-    }
-
-    private void generateTree(final Vector pos, final Vector anchor, final Chunk chunk) {
-        final int type = ThreadLocalRandom.current().nextInt(TREE_TRUNK_HEIGHTS.length);
-        if (anchor.getX() < pos.getX() + TREE_LEAF_RADII[type]
-                || anchor.getZ() < pos.getZ() + TREE_LEAF_RADII[type]
-                || anchor.getX() > pos.getX() + Chunk.SIZE - 1 - TREE_LEAF_RADII[type]
-                || anchor.getY() > pos.getY() + Chunk.SIZE - 1 - TREE_LEAF_RADII[type] - TREE_TRUNK_HEIGHTS[type]
-                || anchor.getZ() > pos.getZ() + Chunk.SIZE - 1 - TREE_LEAF_RADII[type]) {
-            return;
-        }
-        for (final Vector leafPos : Vector.around(Vector.changeY(anchor, TREE_TRUNK_HEIGHTS[type]), TREE_LEAF_RADII[type])) {
-            if (chunk.getBlock(leafPos) instanceof AirBlock) {
-                chunk.putBlock(leafPos, new LeafBlock(leafPos));
-            }
-        }
-        for (final Vector trunkPos : Vector.between(anchor, Vector.changeY(anchor, TREE_TRUNK_HEIGHTS[type]))) {
-            final Block old = chunk.getBlock(trunkPos);
-            if (old instanceof LeafBlock || old instanceof AirBlock) {
-                chunk.putBlock(trunkPos, new WoodBlock(trunkPos));
+    // ##### GENERATION #####
+    private void generate(final Chunk chunk) {
+        for (final Vector blockAnchor : chunk.blockAnchors()) {
+            final double terrainNoise = TERRAIN_NOISE_GENERATOR.noise(blockAnchor);
+            if (terrainNoise > 0.9) {
+                chunk.putBlock(new DirtBlock(blockAnchor));
             }
         }
     }
