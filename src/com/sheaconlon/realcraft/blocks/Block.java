422c56d9ba2a0f50e5de0eb85f4e42b0ecd97513
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-08-06 21:24:32 -0700

    refactoring and bugfixes for Block

--- src/com/sheaconlon/realcraft/blocks/Block.java
+++ src/com/sheaconlon/realcraft/blocks/Block.java
@@ -3,6 +3,7 @@ package com.sheaconlon.realcraft.blocks;
 import com.sheaconlon.realcraft.renderer.Quad;
 import com.sheaconlon.realcraft.renderer.Vertex;
 import com.sheaconlon.realcraft.simulator.Hitbox;
+import com.sheaconlon.realcraft.utilities.ListUtilities;
 import com.sheaconlon.realcraft.utilities.Vector;
 import com.sheaconlon.realcraft.world.WorldObject;
 import com.sheaconlon.realcraft.world.Chunk;
@@ -18,105 +19,74 @@ import java.util.stream.Stream;
  * A block, a cubical, grid-aligned object in the world.
  */
 public abstract class Block extends WorldObject {
-    /**
-     * The normal vector for the front face of a block.
-     */
+    // ##### PUBLIC STATIC FINAL #####
+    public static final int SIZE = 1;
+
+    // ##### PRIVATE STATIC FINAL #####
     private static final float[] FRONT_NORMAL = new float[]{0, 0, 1};
-
-    /**
-     * The normal vector for the left face of a block.
-     */
     private static final float[] LEFT_NORMAL = new float[]{-1, 0, 0};
-
-    /**
-     * The normal vector for the back face of a block.
-     */
     private static final float[] BACK_NORMAL = new float[]{0, 0, -1};
-
-    /**
-     * The normal vector for the right face of a block.
-     */
     private static final float[] RIGHT_NORMAL = new float[]{1, 0, 0};
-
-    /**
-     * The normal vector for the top face of a block.
-     */
     private static final float[] TOP_NORMAL = new float[]{0, 1, 0};
-
-    /**
-     * The normal vector for the bottom face of a block.
-     */
     private static final float[] BOTTOM_NORMAL = new float[]{0, -1, 0};
-
     private static final float[] BLACK = new float[]{0, 0, 0};
-    private static final List<Quad> FACES = Collections.unmodifiableList(Stream.of(
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0,    0,    0}, BLACK, FRONT_NORMAL),
-                    new Vertex(new float[]{0.5f, 0,    0}, BLACK, FRONT_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, 0}, BLACK, FRONT_NORMAL),
-                    new Vertex(new float[]{0,    0.5f, 0}, BLACK, FRONT_NORMAL)
-            ).collect(Collectors.toList())),
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0, 0,    -0.5f}, BLACK, LEFT_NORMAL),
-                    new Vertex(new float[]{0, 0,    0    }, BLACK, LEFT_NORMAL),
-                    new Vertex(new float[]{0, 0.5f, 0    }, BLACK, LEFT_NORMAL),
-                    new Vertex(new float[]{0, 0.5f, -0.5f}, BLACK, LEFT_NORMAL)
-            ).collect(Collectors.toList())),
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0.5f, 0,    -0.5f}, BLACK, BACK_NORMAL),
-                    new Vertex(new float[]{0,    0,    -0.5f}, BLACK, BACK_NORMAL),
-                    new Vertex(new float[]{0,    0.5f, -0.5f}, BLACK, BACK_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, -0.5f}, BLACK, BACK_NORMAL)
-            ).collect(Collectors.toList())),
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0.5f, 0,    0    }, BLACK, RIGHT_NORMAL),
-                    new Vertex(new float[]{0.5f, 0,    -0.5f}, BLACK, RIGHT_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, -0.5f}, BLACK, RIGHT_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, 0    }, BLACK, RIGHT_NORMAL)
-            ).collect(Collectors.toList())),
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0,    0.5f, 0    }, BLACK, TOP_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, 0    }, BLACK, TOP_NORMAL),
-                    new Vertex(new float[]{0.5f, 0.5f, -0.5f}, BLACK, TOP_NORMAL),
-                    new Vertex(new float[]{0,    0.5f, -0.5f}, BLACK, TOP_NORMAL)
-            ).collect(Collectors.toList())),
-            new Quad(Stream.of(
-                    new Vertex(new float[]{0,    0, -0.5f}, BLACK, BOTTOM_NORMAL),
-                    new Vertex(new float[]{0.5f, 0, -0.5f}, BLACK, BOTTOM_NORMAL),
-                    new Vertex(new float[]{0.5f, 0, 0    }, BLACK, BOTTOM_NORMAL),
-                    new Vertex(new float[]{0,    0, 0    }, BLACK, BOTTOM_NORMAL)
-            ).collect(Collectors.toList()))
-    ).collect(Collectors.toList()));
+    private static final List<Quad> FACES = ListUtilities.unmodifiableList(
+            new Quad(
+                    new Vertex(new float[]{ 0,      0, 0}, BLACK, FRONT_NORMAL),
+                    new Vertex(new float[]{SIZE,    0, 0}, BLACK, FRONT_NORMAL),
+                    new Vertex(new float[]{SIZE, SIZE, 0}, BLACK, FRONT_NORMAL),
+                    new Vertex(new float[]{ 0,   SIZE, 0}, BLACK, FRONT_NORMAL)
+            ),
+            new Quad(
+                    new Vertex(new float[]{ 0,    0, -SIZE}, BLACK, LEFT_NORMAL),
+                    new Vertex(new float[]{ 0,    0,     0}, BLACK, LEFT_NORMAL),
+                    new Vertex(new float[]{ 0, SIZE,     0}, BLACK, LEFT_NORMAL),
+                    new Vertex(new float[]{ 0, SIZE, -SIZE}, BLACK, LEFT_NORMAL)
+            ),
+            new Quad(
+                    new Vertex(new float[]{SIZE,    0, -SIZE}, BLACK, BACK_NORMAL),
+                    new Vertex(new float[]{   0,    0, -SIZE}, BLACK, BACK_NORMAL),
+                    new Vertex(new float[]{   0, SIZE, -SIZE}, BLACK, BACK_NORMAL),
+                    new Vertex(new float[]{SIZE, SIZE, -SIZE}, BLACK, BACK_NORMAL)
+            ),
+            new Quad(
+                    new Vertex(new float[]{ SIZE,    0,     0}, BLACK, RIGHT_NORMAL),
+                    new Vertex(new float[]{ SIZE,    0, -SIZE}, BLACK, RIGHT_NORMAL),
+                    new Vertex(new float[]{ SIZE, SIZE, -SIZE}, BLACK, RIGHT_NORMAL),
+                    new Vertex(new float[]{ SIZE, SIZE,     0}, BLACK, RIGHT_NORMAL)
+            ),
+            new Quad(
+                    new Vertex(new float[]{   0, SIZE,     0}, BLACK, TOP_NORMAL),
+                    new Vertex(new float[]{SIZE, SIZE,     0}, BLACK, TOP_NORMAL),
+                    new Vertex(new float[]{SIZE, SIZE, -SIZE}, BLACK, TOP_NORMAL),
+                    new Vertex(new float[]{   0, SIZE, -SIZE}, BLACK, TOP_NORMAL)
+            ),
+            new Quad(
+                    new Vertex(new float[]{   0, 0, -SIZE}, BLACK, BOTTOM_NORMAL),
+                    new Vertex(new float[]{SIZE, 0, -SIZE}, BLACK, BOTTOM_NORMAL),
+                    new Vertex(new float[]{SIZE, 0,     0}, BLACK, BOTTOM_NORMAL),
+                    new Vertex(new float[]{   0, 0,     0}, BLACK, BOTTOM_NORMAL)
+            )
+    );
+    private static final Vector INITIAL_VELOCITY = new Vector(0, 0, 0);
+    private static final double INITIAL_ORIENTATION = 0;
+    private static final Vector HITBOX_POSITION = new Vector(-0.5, -0.5, -0.5);
+    private static final Vector HITBOX_DIMENSIONS = new Vector(1, 1, 1);
 
-    /**
-     * The initial velocity of a block.
-     */
-    private static final Vector INIT_VELOCITY = new Vector(0, 0, 0);
-
-    /**
-     * The initial xz orientation of a block.
-     */
-    private static final double INIT_ORIENT = 0;
-
+    // ##### PRIVATE FINAL #####
     private final List<Hitbox> hitboxes;
-
-    public static final int SIZE_NUMERATOR = 1;
-    public static final int SIZE_DENOMINATOR = 2;
-    public static final double SIZE = (double)SIZE_NUMERATOR / (double)SIZE_DENOMINATOR;
 
     /**
      * Create a block.
-     * @param pos See {@link WorldObject#getPos()}.
+     * @param anchor See {@link WorldObject#getPos()}.
      */
-    public Block(final Vector pos) {
-        super(pos, INIT_ORIENT, INIT_VELOCITY);
-        this.hitboxes = new LinkedList<>();
-        this.hitboxes.add(new Hitbox(this, new Vector(-0.25, -0.25, -0.25), new Vector(0.5, 0.5, 0.5)));
+    public Block(final Vector anchor) {
+        super(anchor, INITIAL_ORIENTATION, INITIAL_VELOCITY);
+        this.hitboxes = ListUtilities.unmodifiableList(
+                new Hitbox(this, HITBOX_POSITION, HITBOX_DIMENSIONS)
+        );
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public List<Vertex> getVertices() {
         final List<Vertex> vertices = new LinkedList<>();
@@ -138,6 +108,6 @@ public abstract class Block extends WorldObject {
 
     @Override
     public List<Hitbox> getHitboxes() {
-        return Collections.unmodifiableList(this.hitboxes);
+        return this.hitboxes;
     }
 }
