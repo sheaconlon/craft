c53d6acd0d918e14d4f49a65affda83edf02c922
Shea Conlon <sheaconlon@users.noreply.github.com>
2017-09-26 12:14:34 -0700

    refactor LRUCache

--- src/com/sheaconlon/realcraft/utilities/LRUCache.java
+++ src/com/sheaconlon/realcraft/utilities/LRUCache.java
@@ -4,22 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * An LRU cache.
+ * An LRU cache. Associates keys with values. Has a limited capacity. Evicts the least recently used entry.
  */
 public class LRUCache<K, V> {
-    private class Entry {
-        final K key;
-        final V value;
+    private static class Entry<EK, EV> {
+        private final EK key;
+        private final EV value;
 
-        public Entry(final K key, final V value) {
+        Entry(final EK key, final EV value) {
             this.key = key;
             this.value = value;
+        }
+
+        EK key() {
+            return this.key;
+        }
+
+        EV value() {
+            return this.value;
         }
     }
 
     private final int capacity;
-    private final Map<K, LinkNode<LRUCache<K, V>.Entry>> keyNodeMap;
-    private final ExposedLinkedList<LRUCache<K, V>.Entry> nodeList;
+    private final Map<K, LinkNode<Entry<K, V>>> keyToNodeMap;
+    private final ExposedLinkedList<Entry<K, V>> queue;
 
     /**
      * Create an LRU cache.
@@ -27,51 +35,52 @@ public class LRUCache<K, V> {
      */
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.keyNodeMap = new HashMap<>();
-        this.nodeList = new ExposedLinkedList<>();
+        this.keyToNodeMap = new HashMap<>();
+        this.queue = new ExposedLinkedList<>();
     }
 
     /**
-     * Get the value for some key.
+     * Get the value associated with some key.
      * @param key The key.
-     * @return The value.
+     * @return The value. Null if there no value associated with {@code key}.
      */
     public V get(final K key) {
-        final LinkNode<LRUCache<K, V>.Entry> node = this.keyNodeMap.get(key);
+        final LinkNode<Entry<K, V>> node = this.keyToNodeMap.get(key);
         if (node == null) {
             return null;
         }
         this.remove(node);
-        this.add(key, node.contents().value);
-        return node.contents().value;
+        this.add(node);
+        return node.contents().value();
     }
 
     /**
-     * Set the value associated with some key, adding it if not already present.
+     * Set the value associated with some key, adding an entry for the key if not present.
      * @param key The key.
-     * @param value The new value.
+     * @param value The value.
      */
     public void put(final K key, final V value) {
-        final LinkNode<LRUCache<K, V>.Entry> node = this.keyNodeMap.get(key);
-        if (node != null) {
+        LinkNode<Entry<K, V>> node = this.keyToNodeMap.get(key);
+        if (node == null) {
+            final Entry<K, V> newEntry = new Entry<>(key, value);
+            node = new LinkNode<>(newEntry);
+        } else {
             this.remove(node);
         }
-        this.add(key, value);
+        this.add(node);
     }
 
-    private void add(final K key, final V value) {
-        if (this.keyNodeMap.size() >= this.capacity) {
-            final LinkNode<LRUCache<K, V>.Entry> nodeToRemove = this.nodeList.removeFront();
+    private void add(final LinkNode<Entry<K, V>> node) {
+        if (this.keyToNodeMap.size() >= this.capacity) {
+            final LinkNode<Entry<K, V>> nodeToRemove = this.queue.removeFront();
             this.remove(nodeToRemove);
         }
-        final LRUCache<K, V>.Entry entry = new LRUCache<K, V>.Entry(key, value);
-        LinkNode<LRUCache<K, V>.Entry> node = new LinkNode<>(entry);
-        this.keyNodeMap.put(key, node);
-        this.nodeList.addBack(node);
+        this.keyToNodeMap.put(node.contents().key(), node);
+        this.queue.addBack(node);
     }
 
-    private void remove(final LinkNode<LRUCache<K, V>.Entry> node) {
+    private void remove(final LinkNode<Entry<K, V>> node) {
         node.remove();
-        this.keyNodeMap.remove(node.contents().key);
+        this.keyToNodeMap.remove(node.contents().key);
     }
 }
